75f66c86ea935c5ec5e223a2470552e1f54e5173
Abdulrahman Qabbout <abdulrahman.qabbout@gmail.com>
2022-04-11 09:36:27 +0300

    Initial Commit

---
+++ CleanFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanFood
+//
+//  Created by Abdulrahman on 4/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
