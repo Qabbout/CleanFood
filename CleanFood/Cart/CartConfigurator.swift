54b1ae218919b5ff66ed323bb86e2ccb7c35ab2e
Abdulrahman Qabbout <abdulrahman.qabbout@gmail.com>
2022-04-19 10:41:49 +0300

    added cart scene initial files and configuurator

---
+++ CleanFood/Cart/CartConfigurator.swift
@@ -0,0 +1,77 @@
+//
+//  CartConfigurator.swift
+//  CleanFood
+//
+//  Created by Abdulrahman on 4/19/22.
+//
+
+
+import UIKit
+
+
+    // View Controller -> Interactor
+protocol CartInteractorConfiguration {
+//    func workerGetCategories(request: Home.RequestCategories)
+//    func workerGetItemsOfACategory(request: Home.RequestItemsOfACategory)
+
+}
+
+
+    // Interactor -> Presenter
+protocol CartPresenterConfiguration {
+//    func presentCategories(response: Home.ResponseCategories)
+//    func presentItemsOfACategory(response: Home.ResponseItemsOfACategory)
+}
+
+    // Presentor -> View Controller
+protocol CartDisplayConfiguration: AnyObject {
+//    func displayCategories(viewModel: Home.ViewModelCategories)
+//    func displayItemsOfACategory(viewModel: Home.ViewModelItemsOfACategory)
+
+}
+
+    // Interactor -> Worker
+protocol CartWorkerConfiguration {
+//    func doSomeWork()
+}
+
+
+    // View Controller -> Route
+protocol CartRoutingConfiguration {
+        //    func routeToCart()
+}
+
+protocol CartDataPassing {
+    var dataStore: CartDataStore? { get }
+}
+
+    // Data Store of Route
+protocol CartDataStore {
+//    var items: Items? { get }
+}
+
+
+
+class CartConfigurator {
+
+    static let shared = CartConfigurator()
+
+    private init() { }
+
+    func configure(viewController: CartViewController)
+    {
+    let router = CartRouter()
+    router.viewController = viewController
+
+    let presenter = CartPresenter()
+    presenter.viewController = viewController
+
+    let interactor = CartInteractor()
+    interactor.presenter = presenter
+
+    router.dataStore = interactor
+
+    viewController.interactor = interactor
+    viewController.router = router
+    }
+}
